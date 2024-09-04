a49ce046d6a744a2ad954c75d1216ea431dad0b4
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-09-04 19:02:30 -0400

    ...

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import menu from "../assets/open_menu.svg";
+
+const Header = () => {
+  return (
+   <header>
+    <span>WESTON RWIGEMA</span>
+    <button className='menu-btn'>
+    <img src={menu} alt="menu"/>
+    </button> 
+    < nav className="nav">
+      <ul>
+      <li>
+        <a href ="#about">About</a>
+      </li>
+      <li>
+         <a href="#projects">Projects</a>
+      </li>
+      <li>
+       <a href="contact">Contact</a>
+      </li>
+      </ul>
+     </nav>
+   </header>
+  )
+}
+
+export default Header
