2b47228e33bfd67cff36aaef89bac91b2caa4e99
Weston Rwigema <161762685+WeStOn2000@users.noreply.github.com>
2024-10-16 00:54:45 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero  from "./components/Hero";
+import { Hero }  from "./components/hero";
 import { Projects } from "./components/Projects";
 
 const App = () => {
